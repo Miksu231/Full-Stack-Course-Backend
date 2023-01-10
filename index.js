3caa9c54278e792b0160a0086daad56b9fed08c2
lainem25 <mikael.2.laine@aalto.fi>
2023-01-10 23:38:50 +0200

    Completed 3.7-3.8

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+
+morgan.token('data', function (req, res) {
+  if(req.method === 'POST') return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 let persons = [
     { 
       "id": 1,
